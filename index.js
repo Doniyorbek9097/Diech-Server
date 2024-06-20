477b25039b78dc9f7abe08eaacc78d9b584efa2a
Doniyorbek9097 <doniyorbek3322@mail.ru>
2024-06-20 15:04:46 +0500

    yangilash

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const io = new Server(server, {
 app.use(cors());
 app.use(bodyParser.json({limit: '100mb'}));
 app.use(bodyParser.urlencoded({limit: '100mb', extended: true}));
-app.use("/uploads", express.static("uploads"));
+app.use("/uploads", express.static('mnt/data/uploads'));
 app.use("/", (req, res, next) => {
     const lang = req.headers['lang']
     if(lang) mongoose.setDefaultLanguage(lang);
