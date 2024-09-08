73a09a43d5ea067f765a392b792d13dfcee33e36
Doniyorbek9097 <doniyorbek3322@mail.ru>
2024-09-09 00:33:25 +0500

    added

--- routes/client/subscription.js
+++ routes/client/subscription.js
@@ -1,5 +1,4 @@
-
-const subscriptionModel = require("../../models/subscription.model")
+const subscriptionModel = require("../../models/subscription.model");
 const webPush = require('web-push');
 
 const vapidKeys = webPush.generateVAPIDKeys();
@@ -9,20 +8,19 @@ webPush.setVapidDetails(
     "kdo9-vksU-zvU1zhMLD8yNGdj45gRMITiNh35g2wtMc"
 );
 
-
 const subscriptionRoutes = async (fastify, options) => {
     // Foydalanuvchini obuna qilish va ma'lumotlarni bazaga saqlash
     fastify.post('/subscribe', async (req, res) => {
         const subscription = req.body.subscription; // Foydalanuvchi subscription obyektini olamiz
 
-        // Yangi obunani saqlash
         try {
             // Obuna mavjudligini tekshirish
             const existingSubscription = await subscriptionModel.findOne({ 'endpoint': subscription.endpoint });
 
             if (existingSubscription) {
-                // Agar obuna mavjud bo'lsa
-                return res.status(200).send({ success: true, message: 'Obuna allaqachon mavjud' });
+                // Agar obuna mavjud bo'lsa, yangilang
+                await subscriptionModel.updateOne({ 'endpoint': subscription.endpoint }, subscription);
+                return res.status(200).send({ success: true, message: 'Obuna yangilandi' });
             }
             const newSubscription = await new subscriptionModel(subscription).save(); // MongoDB ga saqlash
             res.status(200).send({ success: true, message: 'Foydalanuvchi obunasi saqlandi' });
@@ -31,8 +29,6 @@ const subscriptionRoutes = async (fastify, options) => {
             res.status(500).send({ success: false, message: 'Obunani saqlab bo\'lmadi' });
         }
     });
-
-
 
     // Saqlangan obunalarga push xabar yuborish
     fastify.get('/send-push', async (req, res) => {
@@ -46,15 +42,18 @@ const subscriptionRoutes = async (fastify, options) => {
             const subscriptions = await subscriptionModel.find();
 
             // Har bir obunaga push xabar yuborish
-            subscriptions.forEach(subscription => {
-                webPush.sendNotification(subscription, payload)
-                    .then(() => {
-                        console.log('Push xabar yuborildi');
-                    })
-                    .catch(error => {
-                        console.error('Push xabar yuborishda xatolik:', error);
-                    });
-            });
+            for (const subscription of subscriptions) {
+                try {
+                    await webPush.sendNotification(subscription, payload);
+                    console.log('Push xabar yuborildi');
+                } catch (error) {
+                    console.error('Push xabar yuborishda xatolik:', error);
+                    // Eskirgan yoki noto'g'ri obunani o'chirish
+                    if (error.statusCode === 410) {
+                        await subscriptionModel.deleteOne({ 'endpoint': subscription.endpoint });
+                    }
+                }
+            }
 
             res.status(200).send({ success: true, message: 'Xabarlar yuborildi' });
         } catch (error) {
@@ -62,7 +61,6 @@ const subscriptionRoutes = async (fastify, options) => {
             res.status(500).send({ success: false, message: 'Xabarlar yuborilmadi' });
         }
     });
-
-}
+};
 
 module.exports = subscriptionRoutes;
