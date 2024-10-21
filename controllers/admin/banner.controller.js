6a2bf176205ecc3edebbd59bd6c1b7c1eeaf784f
Doniyorbek9097 <doniyorbek3322@mail.ru>
2024-10-21 22:19:17 +0500

    Initial commit

--- controllers/admin/banner.controller.js
+++ controllers/admin/banner.controller.js
@@ -6,10 +6,32 @@ class Banner {
     async create(req, reply) {
         const { image, slug } = req.body;
         try {
-            const result = await new bannerModel(req.body).save();
-            
+            const newBanner = await new bannerModel(req.body).save();
+
+            if (newBanner.image) {
+                await fileModel.updateOne(
+                    { image_url: newBanner.image.uz },
+                    { isActive: true, owner_id: newProduct._id, owner_type: "banner" },
+                );
+                await fileModel.updateOne(
+                    { image_url: newBanner.image.ru },
+                    { isActive: true, owner_id: newProduct._id, owner_type: "banner" },
+                );
+            }
+
+            if (newBanner.smallImage) {
+                await fileModel.updateOne(
+                    { image_url: newBanner.smallImage.uz },
+                    { isActive: true, owner_id: newProduct._id, owner_type: "banner" },
+                );
+                await fileModel.updateOne(
+                    { image_url: newBanner.smallImage.ru },
+                    { isActive: true, owner_id: newProduct._id, owner_type: "banner" },
+                );
+            }
+
             return reply.send({
-                data: result,
+                data: newBanner,
                 message: "Success"
             });
 
@@ -23,14 +45,14 @@ class Banner {
 
 
     async all(req, reply) {
-        try {       
+        try {
             let result = await bannerModel.find();
-            
-           return reply.send({
-             data: result,
-             message:"Success"
-           });
-           
+
+            return reply.send({
+                data: result,
+                message: "Success"
+            });
+
         } catch (error) {
             console.log(error);
             return reply.status(500).send(error.message)
@@ -43,13 +65,13 @@ class Banner {
             const result = await bannerModel.findByIdAndDelete(req.params.id).lean()
             const { image } = result;
             image.uz && await fileService.remove(image.uz)
-            image.ru && await fileService.remove(image.ru)    
-    
-            return reply.send({ data: result, message:"Success"});
-    
+            image.ru && await fileService.remove(image.ru)
+
+            return reply.send({ data: result, message: "Success" });
+
         } catch (error) {
             console.log(error)
-            return  reply.status(500).send(error.message)
+            return reply.status(500).send(error.message)
         }
     }
 
---
+++ routes/admin/file.route.js
@@ -0,0 +1,14 @@
+const { checkToken } = require("../../middlewares/authMiddleware");
+const fileController = require("../../controllers/file.controller")
+
+
+const fileRoutes = async (fastify, options) => {
+    fastify.post("/image-upload", { preHandler: checkToken }, fileController.imageUpload)
+    fastify.delete("/image-remove/:image_url", { preHandler: checkToken }, fileController.imageRemove)
+}
+
+
+
+
+module.exports = fileRoutes;
+
